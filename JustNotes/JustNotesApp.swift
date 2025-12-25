e6a64dd57b54778e1b88d882836cae1f539df59f
avayunus <avayunus02@gmail.com>
2025-12-25 14:38:55 -0700

    Initial Commit

---
+++ JustNotes/JustNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustNotesApp.swift
+//  JustNotes
+//
+//  Created by Ava Yunus on 2025-12-25.
+//
+
+import SwiftUI
+
+@main
+struct JustNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
